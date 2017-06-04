c7726b811a999630c1ae72554fb4308b1f78d0c4
jltajada <joseluistajada@gmail.com>
2017-06-04 19:14:08 +0200

    Version 1
    Primera versión de la librería para acceder a los registros del P4292
    mediante I2C

---
+++ lib/Qi-P9242/Qi9242.cpp
@@ -0,0 +1,153 @@
+/*
+   Librería para acceder por I2C (400KHz) a los registros del módulo Qi P9242-R de IDT Inc.
+   Creado por Jose Luis Tajada, Junio 2017
+   joseluistajada@gmail.com
+   jltajada@chargy.com
+ */
+
+
+
+#include "Qi9242.h"
+
+
+Qi9242::Qi9242(int addres) // define la dirección i2C del puerto del P9242-R
+{
+        _Qi_addres=addres;
+}
+
+
+int Qi9242::state()// pregunta por el estado del Qi
+{
+        Wire.beginTransmission(_Qi_addres); // selecciona la dirección del dispositivo Qi9242----------------------------------
+        //delay(100);
+        Wire.write( highByte( ADDR_STATE));
+        Wire.write(  lowByte( ADDR_STATE));
+        //delay(100);
+        Wire.endTransmission();
+        //delay(100);
+        Wire.requestFrom(_Qi_addres, 1); // longitud en bits de la respuesta
+        _n =Wire.read();
+        return _n;
+}
+
+
+int Qi9242::status()// pregunta por el estatus operativo del Qi
+{
+        Wire.beginTransmission(_Qi_addres); // selecciona la dirección del dispositivo Qi9242----------------------------------
+        //delay(100);
+        Wire.write( highByte( ADDR_STATUS));
+        Wire.write(  lowByte( ADDR_STATUS));
+        //delay(100);
+        Wire.endTransmission();
+        //delay(100);
+        Wire.requestFrom(_Qi_addres, 1); // longitud en bits de la respuesta
+        _n =Wire.read();
+        return _n;
+}
+
+
+int Qi9242::coil_current()// pregunta por la corriente aplicada a la coil del Qi
+{
+        Wire.beginTransmission(_Qi_addres); // selecciona la dirección del dispositivo Qi9242----------------------------------
+        //delay(100);
+        Wire.write( highByte( ADDR_COIL_CURRENT));
+        Wire.write(  lowByte( ADDR_COIL_CURRENT));
+        //delay(100);
+        Wire.endTransmission();
+        //delay(100);
+        Wire.requestFrom(_Qi_addres, 2); // longitud en bits de la respuesta
+        _n =Wire.read();
+        _m =Wire.read();
+        _p =(_m<<8) | _n;
+        //Serial.println(String(_n)+" "+String(_m)+" "+String(_p));
+        return _p;
+}
+
+
+int Qi9242::coil_voltage()// pregunta por el valor del voltaje aplicado a la coil del Qi
+{
+        Wire.beginTransmission(_Qi_addres); // selecciona la dirección del dispositivo Qi9242----------------------------------
+        //delay(100);
+        Wire.write( highByte( ADDR_COIL_VOLTAGE));
+        Wire.write(  lowByte( ADDR_COIL_VOLTAGE));
+        //delay(100);
+        Wire.endTransmission();
+        //delay(100);
+        Wire.requestFrom(_Qi_addres, 2); // longitud en bits de la respuesta
+        _n =Wire.read();
+        _m =Wire.read();
+        _p =(_m<<8) | _n;
+        //Serial.println(String(_n)+" "+String(_m)+" "+String(_p));
+        return _p;
+}
+
+
+float Qi9242::sensing_voltage()// pregunta por el valor del voltaje del termistor
+{
+        Wire.beginTransmission(_Qi_addres); // selecciona la dirección del dispositivo Qi9242----------------------------------
+        //delay(100);
+        Wire.write( highByte( ADDR_SENSING_VOLTAGE));
+        Wire.write(  lowByte( ADDR_SENSING_VOLTAGE));
+        //delay(100);
+        Wire.endTransmission();
+        //delay(100);
+        Wire.requestFrom(_Qi_addres, 2); // longitud en bits de la respuesta
+        _n =Wire.read();
+        _m =Wire.read();
+        _r =( ((_m<<8) | _n)*2.4 )/4095;
+        //Serial.println(String(_n)+" "+String(_m)+" "+String(_p));
+        return _r;
+}
+
+
+int Qi9242::frequency()// pregunta por el valor de la frecuencia de trabajo del circuito resonante
+{
+        Wire.beginTransmission(_Qi_addres); // selecciona la dirección del dispositivo Qi9242----------------------------------
+        //delay(100);
+        Wire.write( highByte( ADDR_FREQUENCY));
+        Wire.write(  lowByte( ADDR_FREQUENCY));
+        //delay(100);
+        Wire.endTransmission();
+        //delay(100);
+        Wire.requestFrom(_Qi_addres, 2); // longitud en bits de la respuesta
+        _n =Wire.read();
+        _m =Wire.read();
+        _p =((_m<<8) | _n);
+        _p=60000/_p;
+        //Serial.println(String(_n)+" "+String(_m)+" "+String(_p)+" "+String(_r));
+        return _p;
+}
+
+
+int Qi9242::operating_duty()// pregunta por el valor del ciclo activo del circuito resonante
+{
+        Wire.beginTransmission(_Qi_addres); // selecciona la dirección del dispositivo Qi9242----------------------------------
+        //delay(100);
+        Wire.write( highByte( ADDR_OPERATING_DUTY));
+        Wire.write(  lowByte( ADDR_OPERATING_DUTY));
+        //delay(100);
+        Wire.endTransmission();
+        //delay(100);
+        Wire.requestFrom(_Qi_addres, 2); // longitud en bits de la respuesta
+        _n =Wire.read();
+        _m =Wire.read();
+        _p =((_m<<8) | _n);
+        _p=(_p*50)/255;
+        //Serial.println(String(_n)+" "+String(_m)+" "+String(_p)+" "+String(_r));
+        return _p;
+}
+
+
+int Qi9242::bridge_status()// pregunta por el tipo de configuración del Puente de los MosFet
+{
+        Wire.beginTransmission(_Qi_addres); // selecciona la dirección del dispositivo Qi9242----------------------------------
+        //delay(100);
+        Wire.write( highByte( ADDR_BRIDGE_STATUS));
+        Wire.write(  lowByte( ADDR_BRIDGE_STATUS));
+        //delay(100);
+        Wire.endTransmission();
+        //delay(100);
+        Wire.requestFrom(_Qi_addres, 1); // longitud en bits de la respuesta
+        _n =Wire.read();
+        return _n;
+}
---
+++ lib/Qi-P9242/Qi9242.h
@@ -0,0 +1,48 @@
+/*
+   Librería para acceder por I2C (400KHz) a los registros del módulo Qi P9242-R de IDT Inc.
+   Creado por Jose Luis Tajada, Junio 2017
+   joseluistajada@gmail.com
+   jltajada@chargy.com
+ */
+
+
+#ifndef Qi9242_h
+#define Qi9242_h
+
+#include "Wire.h"
+#include "Arduino.h"
+
+#define ADDR_STATE 0X6E0 // definición de la dirección de los registros internos del P4292-R
+#define ADDR_STATUS 0X6E1
+#define ADDR_COIL_CURRENT 0X6E2
+#define ADDR_COIL_VOLTAGE 0X6E4
+#define ADDR_SENSING_VOLTAGE  0X6E8
+#define ADDR_FREQUENCY 0X6EA
+#define ADDR_OPERATING_DUTY 0X6EC
+#define ADDR_BRIDGE_STATUS 0X6EE
+
+
+class Qi9242
+{
+public:
+Qi9242(int addres);
+int state();
+int status();
+int coil_current();
+int coil_voltage();
+float sensing_voltage();
+int frequency();
+int operating_duty();
+int bridge_status();
+
+private:
+int _Qi_addres;
+int _register_addr;
+int _register_long;
+int _n;
+int _m;
+unsigned int _p;
+float _r;
+};
+
+#endif
